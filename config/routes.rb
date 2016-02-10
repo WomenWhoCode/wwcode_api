83771e5afbbef50a3c37c8eddc842b21b05e0fb0
Samantha Morco <morkotiks@gmail.com>
2016-02-09 21:44:10 -0800

    updated routes on user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # mount_devise_token_auth_for 'User', at: 'auth'
+  # devise_for :users
+  mount_devise_token_auth_for 'User', at: 'auth'
   root to: "welcome#index"
 
   namespace :api do
