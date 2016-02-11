8c5ff618bd5bdb34268fe1903ab9b633b809d4f3
Tim Chai <timothy.y.chai@gmail.com>
2016-02-11 09:52:35 -0800

    added event model and actions

---
+++ app/controllers/api/v1/events_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = Event.create(objectId: params[:objectId], featured: params[:featured], network: params[:network], updatedAt: params[:updatedAt], url: params[:url], event_date: params[:event_date],location: params[:location], title: params[:title], subscribe_count: params[:subscribe_count], createdAt: params[:createdAt], description: params[:description], meetup_event_id: params[:meetup_event_id], time_zone: params[:time_zone], hex_color: params[:hex_color])
+    render :show
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.updated(objectId: params[:objectId], featured: params[:featured], network: params[:network], updatedAt: params[:updatedAt], url: params[:url], event_date: params[:event_date],location: params[:location], title: params[:title], subscribe_count: params[:subscribe_count], createdAt: params[:createdAt], description: params[:description], meetup_event_id: params[:meetup_event_id], time_zone: params[:time_zone], hex_color: params[:hex_color])
+    render :show
+  end
+
+  def destroy
+    Event.find(params[:id]).destroy
+    format.json {render json: "Event Removed"}
+  end
+
+end
