ce71aa07d6c968d76a8a1cad958f788082c3f8ea
elaine <hbotelloelaine@gmail.com>
2016-02-10 22:59:39 -0800

    test

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160208053738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "personalization_questions", force: :cascade do |t|
+    t.string   "objectId"
+    t.string   "detail"
+    t.date     "createdAt"
+    t.date     "updatedAt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
---
+++ spec/factories/personalization_questions.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :personalization_question do
+    objectId "MyString"
+    detail "MyString"
+    createdAt "2016-02-07"
+    updatedAt "2016-02-07"
+  end
+end
