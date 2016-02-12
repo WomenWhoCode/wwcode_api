79952b1e681014423b38158d4c37dfbeaf5b8a4b
elaine <hbotelloelaine@gmail.com>
2016-02-11 21:43:27 -0800

    test

--- db/schema.rb
+++ db/schema.rb
@@ -11,53 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160208053738) do
+ActiveRecord::Schema.define(version: 20160210024556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-<<<<<<< HEAD
-=======
-  create_table "users", force: :cascade do |t|
-    t.string   "provider",               default: "email", null: false
-    t.string   "uid",                    default: "",      null: false
-    t.string   "encrypted_password",     default: "",      null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,       null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.string   "name"
-    t.string   "nickname"
-    t.string   "image"
-    t.string   "email"
-    t.json     "tokens"
-    t.string   "username",               default: "",      null: false
-    t.boolean  "emailVerified",          default: false
-    t.integer  "profile_id"
-    t.string   "phone",                  default: "",      null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
-
 
   create_table "personalization_questions", force: :cascade do |t|
     t.string   "objectId"
     t.string   "detail"
     t.date     "createdAt"
     t.date     "updatedAt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
+  create_table "profiles", force: :cascade do |t|
+    t.string   "objectId"
+    t.string   "full_name"
+    t.string   "image_url"
+    t.string   "job_title"
+    t.decimal  "latitude",   precision: 15, scale: 10
+    t.decimal  "longitude",  precision: 15, scale: 10
+    t.string   "network"
+    t.string   "photo"
+    t.integer  "theme_type"
+    t.integer  "userId"
+    t.datetime "createdAt"
+    t.datetime "updatedAt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -95,5 +75,4 @@ ActiveRecord::Schema.define(version: 20160208053738) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
 
->>>>>>> 5b7d845eacbcb5b95289d9d9fcb81de7b0445c1a
 end
