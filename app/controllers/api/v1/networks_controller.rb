afd8e35a867aa1b01ee8f1071d8d80b98934cf1f
Samantha Morco <morkotiks@gmail.com>
2016-02-15 18:44:14 -0800

    updated network controller to include Geocoder

--- app/controllers/api/v1/networks_controller.rb
+++ app/controllers/api/v1/networks_controller.rb
@@ -8,14 +8,20 @@ class Api::V1::NetworksController < ApplicationController
     @network = Network.find(params[:id])
   end
 
-  def create
-    @network = Network.create(objectId: params[:objectId], awesome_count: params[:awesome_count], image_url: params[:image_url], latitude: params[:latitude], longitude: params[:longitude], meetup_group_id: params[:meetup_group_id], time_zone: params[:time_zone], title: params[:title], createdAt: params[:createdAt], updatedAt: params[:updatedAt])
+  def create    
+    coordinates = Geocoder.coordinates(params[:location])
+    latitude = coordinates[0]
+    longitude = coordinates [1]
+    @network = Network.create(objectId: params[:objectId], awesome_count: params[:awesome_count], image_url: params[:image_url], latitude: latitude, longitude: longitude, meetup_group_id: params[:meetup_group_id], time_zone: params[:time_zone], title: params[:title], createdAt: params[:createdAt], updatedAt: params[:updatedAt])
     render :show
   end
 
   def update
     @network = Network.find(params[:id])
-    @network.update(objectId: params[:objectId], awesome_count: params[:awesome_count], image_url: params[:image_url], latitude: params[:latitude], longitude: params[:longitude], meetup_group_id: params[:meetup_group_id], time_zone: params[:time_zone], title: params[:title], updatedAt: params[:updatedAt])
+    coordinates = Geocoder.coordinates(params[:location])
+    latitude = coordinates[0]
+    longitude = coordinates [1]
+    @network.update(objectId: params[:objectId], awesome_count: params[:awesome_count], image_url: params[:image_url], latitude: latitude, longitude: longitude, meetup_group_id: params[:meetup_group_id], time_zone: params[:time_zone], title: params[:title], updatedAt: params[:updatedAt])
     render :show
   end
 
