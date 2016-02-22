f13babb786e8aaf1a7d9fd7e28604b063710892a
elaine <hbotelloelaine@gmail.com>
2016-02-22 15:21:14 -0800

    update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   include DeviseTokenAuth::Concerns::User
-<<<<<<< HEAD
-=======
   has_many :subscribes
-<<<<<<< HEAD
->>>>>>> 3b66ce485b782af4aba0c14478d8f314fa29b142
-=======
   has_one :profile
->>>>>>> d645854ae381c97b99899a68d4893589367c50fa
 end
