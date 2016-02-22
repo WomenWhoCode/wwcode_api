f9f8818db877d66c337b72aa21be9154d8fed589
elaine <hbotelloelaine@gmail.com>
2016-02-22 08:50:10 -0800

    updated controller and views

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,31 +1,32 @@
 class Api::V1::ProfilesController < ApplicationController
- def index
-  @profiles = Profile.all
-end
+  before_filter :authenticate_user!, except: [:index, :show]
 
-def show
-  @profile = Profile.find(params[:id])
-end
+  def index
+    @profiles = Profile.all
+  end
 
-def create
-  coordinates = Geocoder.coordinates(params[:location])
-  latitude = coordinates[0]
-  longitude = coordinates [1]
-  @profile = Profile.create(objectId: params[:objectId], full_name: params[:full_name],image_url: params[:image_url], job_title: params[:job_title], latitude: params[:latitude], longitude: params[:longitude], network: params[:network], photo: params[:photo], theme_type: params[:theme_type], userId: params[:userId], createdAt: params[:creaedAt], updatedAt: parms[:updatedAt])
-  render :show
-end
+  def show
+    @profile = Profile.find(params[:id])
+  end
 
-def update
-  @profile = Profile.find(params[:id])
-  coordinates = Geocoder.coordinates(params[:location])
-  latitude = coordinates[0]
-  longitude = coordinates [1]
-  @profile.update(objectId: params[:objectId], full_name: params[:full_name],image_url: params[:image_url], job_title: params[:job_title], latitude: params[:latitude], longitude: params[:longitude], network: params[:network], photo: params[:photo], theme_type: params[:theme_type], userId: params[:userId], createdAt: params[:createdAt], updatedAt: params[:updatedAt])
-  render :show
-end
+  def create
+    coordinates = Geocoder.coordinates(params[:location])
+    latitude = coordinates[0]
+    longitude = coordinates [1]
+    @profile = Profile.create(objectId: params[:objectId], full_name: params[:full_name],image_url: params[:image_url], job_title: params[:job_title], latitude: params[:latitude], longitude: params[:longitude], network: params[:network], photo: params[:photo], theme_type: params[:theme_type], user_id: params[:user_id], createdAt: params[:creaedAt], updatedAt: parms[:updatedAt])
+    render :show
+  end
 
-def destroy
-  Profile.find(params[:id]).destroy
-  render json: "Profile Deleted"
-end
-end
+  def update
+    @profile = Profile.find(params[:id])
+    coordinates = Geocoder.coordinates(params[:location])
+    latitude = coordinates[0]
+    longitude = coordinates [1]
+    @profile.update(objectId: params[:objectId], full_name: params[:full_name],image_url: params[:image_url], job_title: params[:job_title], latitude: params[:latitude], longitude: params[:longitude], network: params[:network], photo: params[:photo], theme_type: params[:theme_type], user_id: params[:user_id], createdAt: params[:createdAt], updatedAt: params[:updatedAt])
+    render :show
+  end
+
+  def destroy
+    Profile.find(params[:id]).destroy
+    format.json {render json: "Profile Deleted"}
+  end
