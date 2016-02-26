e1b17178812bc9f9052d8ef13f9a31994409d3e2
Tim Chai <timothy.y.chai@gmail.com>
2016-02-25 23:54:46 -0800

    associated network and profile

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,4 +1,5 @@
 class Network < ActiveRecord::Base
   has_many :events
   has_many :subscribes
+  has_many :profiles
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226011711) do
+ActiveRecord::Schema.define(version: 20160226075139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,10 +64,8 @@ ActiveRecord::Schema.define(version: 20160226011711) do
   create_table "personalization_questions", force: :cascade do |t|
     t.string   "objectId"
     t.string   "detail"
-    t.date     "createdAt"
-    t.date     "updatedAt"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
@@ -99,6 +97,7 @@ ActiveRecord::Schema.define(version: 20160226011711) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "network_id"
   end
 
   create_table "replies", force: :cascade do |t|
