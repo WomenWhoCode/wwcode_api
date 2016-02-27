d56011f9530ea57c7a60c0ed7dd4622c8156dad8
elaine <hbotelloelaine@gmail.com>
2016-02-27 08:05:08 -0800

    updated post controller to show replies

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160227042030) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +64,8 @@ ActiveRecord::Schema.define(version: 20160227042030) do
   create_table "personalization_questions", force: :cascade do |t|
     t.string   "objectId"
     t.string   "detail"
+    t.date     "createdAt"
+    t.date     "updatedAt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
