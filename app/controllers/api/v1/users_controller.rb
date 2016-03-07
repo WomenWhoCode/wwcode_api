3dc0d38b08fd789bec27f63274f55e63d530cbb6
Tim Chai <timothy.y.chai@gmail.com>
2016-03-06 19:59:30 -0800

    updated user and events controllers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -28,20 +28,22 @@ class Api::V1::UsersController < ApplicationController
 
   def user_events
     user_id = params[:user_id]
-    user = User.find(user_id)
-    profile = user.profile
-    network = user.network
-    events = network.events
-    respond_to do |format|
-      format.js do
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-        response.headers['Access-Control-Allow-Methods'] = 'GET,POST,OPTIONS'
-        response.headers['Access-Control-Allow-Origin'] = ''
-        render json: network.to_json(include: :events), :callback => params[:callback]
+      user = User.find(user_id)
+      profile = user.profile
+      network = user.network
+      network_hash = user.network.as_json
+      events = network.events
+    
+      respond_to do |format|
+        format.js do
+          response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+          response.headers['Access-Control-Allow-Methods'] = 'GET,POST,OPTIONS'
+          response.headers['Access-Control-Allow-Origin'] = ''
+          render json: network_hash, :callback => params[:callback]
+        end
+        format.json do
+          render json: network_hash
+        end
       end
-      format.json do
-        render json: network.to_json(include: :events)
-      end
-    end
   end
 end
