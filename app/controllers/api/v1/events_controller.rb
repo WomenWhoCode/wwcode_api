aa75445b493e649a9055ed386b46db1a42763867
Tim Chai <timothy.y.chai@gmail.com>
2016-03-06 20:16:37 -0800

    updated user controller

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -24,28 +24,4 @@ class Api::V1::EventsController < ApplicationController
     format.json {render json: "Event Removed"}
   end
 
-  def user_events
-    user_id = params[:user_id]
-    user = User.find(user_id)
-    profile = user.profile
-    network = user.network
-    events = network.events
-
-    user_hash = user.as_json(
-      include: {network: {
-        include: :events
-        }})
-
-    user_hash["login_location_networks"] = [].as_json(include: :events) #[] to be filled in with location based on login geo-coordinates sent by client side
-
-    respond_to do |format|
-      format.js do
-       response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-       response.headers['Access-Control-Allow-Methods'] = 'GET,POST,OPTIONS'
-       response.headers['Access-Control-Allow-Origin'] = ''
-       render json: user_hash, :callback => params[:callback]
-      end
-    end
-  end
-
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -28,21 +28,22 @@ class Api::V1::UsersController < ApplicationController
 
   def user_events
     user_id = params[:user_id]
-      user = User.find(user_id)
-      profile = user.profile
-      network = user.network
-      network_hash = user.network.as_json
-      events = network.events
+    user = User.find(user_id)
+    profile = user.profile
+    network = user.network
+    events = network.events
     
+    user_hash["login_location_networks"] = [].as_json(include: :events) #[] to be filled in with location based on login geo-coordinates sent by client side
+
       respond_to do |format|
         format.js do
           response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
           response.headers['Access-Control-Allow-Methods'] = 'GET,POST,OPTIONS'
           response.headers['Access-Control-Allow-Origin'] = ''
-          render json: network_hash, :callback => params[:callback]
+          render json: user_hash, :callback => params[:callback]
         end
         format.json do
-          render json: network_hash
+          render json: user_hash
         end
       end
   end
