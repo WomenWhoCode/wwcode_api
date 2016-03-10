539b7b327b2df37a31bf320e4f894b119831b7b4
Tim Chai <timothy.y.chai@gmail.com>
2016-03-09 19:15:02 -0800

    renamed app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ww-code-api_session'
+Rails.application.config.session_store :cookie_store, key: '_wwcode_api_session'
